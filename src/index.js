217e3adda5b559722b88bfd5a889866acc328fb0
Evans <evansroysir@gmail.com>
2023-05-01 07:32:51 +0300

    Displayed fetch data on the page

--- src/index.js
+++ src/index.js
@@ -5,12 +5,19 @@ const init = () => {
     event.preventDefault()
 
     const input = document.querySelector('input#searchByID')
-    console.log(input.value)
+    
 
     fetch(`http://localhost:3000/movies/${input.value}`)
       .then(response => response.json())
       .then(data => {
-        console.log(data)
+        // console.log(data)
+        //Access the DOM and store the two elements in js
+        const title = document.querySelector('section#movieDetails h4')
+        const summary = document.querySelector('section#movieDetails p')
+
+        //Change the content of title and summary based on the retrieve data
+        title.innerText = data.title
+        summary.innerText = data.summary
       })
   })
 }
